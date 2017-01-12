a5305f8684871bf0e14a1dcebe290a2a37db19b8
Jack0805 <zhangke0805@gmail.com>
2017-01-12 21:58:43 +1100

    Initial Commit

---
+++ simpleCRM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleCRM
+//
+//  Created by jack on 12/1/17.
+//  Copyright © 2017 ke zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
